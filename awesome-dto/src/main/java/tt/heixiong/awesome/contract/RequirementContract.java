1a617e3e8125a9e36ffd331558142473e6c114e3
zhaotao <zhaotao4@mychery.com>
2026-03-20 11:04:21 +0800

    fix:修复项目中问题

--- awesome-dto/src/main/java/tt/heixiong/awesome/contract/RequirementContract.java
+++ awesome-dto/src/main/java/tt/heixiong/awesome/contract/RequirementContract.java
@@ -1,12 +1,6 @@
 package tt.heixiong.awesome.contract;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import tt.heixiong.awesome.common.ApiResponse;
 import tt.heixiong.awesome.dto.RequirementDto;
 import tt.heixiong.awesome.req.RequirementCreateReq;
--- awesome-web/src/main/java/tt/heixiong/awesome/client/RequirementClient.java
+++
@@ -1,8 +0,0 @@
-package tt.heixiong.awesome.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import tt.heixiong.awesome.contract.RequirementContract;
-
-@FeignClient(value = "awesome", path = "/requirements")
-public interface RequirementClient extends RequirementContract {
-}
